47862c97a0587af2bbb98a6a020d87cb02c7308b
Lindsen13 <ivo.lindsen@hotmail.com>
2023-03-21 10:31:22 +0100

    uncommited files

--- lib/fargate.ts
+++ lib/fargate.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { aws_ecs as ecs } from 'aws-cdk-lib';
 import { aws_ecs_patterns as ecs_patterns } from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib'
 
 
 export class CdkPipelinesStack extends cdk.Stack {
@@ -11,8 +12,10 @@ export class CdkPipelinesStack extends cdk.Stack {
         // We'll be creating a ECS service with a fargate container running
         // a simple python task to save data to a s2 bucket
 
+        const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
+
         // Creating a cluster
-        const cluster = new ecs.Cluster(this, "ClusterForPipelines");
+        const cluster = new ecs.Cluster(this, "ClusterForPipelines", { vpc: vpc });
 
         // Create a Fargate Task
         const fargateTask = new ecs.FargateTaskDefinition(this, 'HelloWorldTask');
